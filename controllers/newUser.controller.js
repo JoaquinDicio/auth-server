6daa8a81a6879ad77873c0cbc1cad71cc46e2822
joaquin di cio <joacodicio@gmail.com>
2023-12-29 14:27:28 -0300

    manejo de errores

--- controllers/newUser.controller.js
+++ controllers/newUser.controller.js
@@ -1,12 +1,30 @@
-import { doc, setDoc } from "firebase/firestore";
+import {
+  DocumentReference,
+  doc,
+  getDoc,
+  getDocFromCache,
+  setDoc,
+} from "firebase/firestore";
 import getDb from "../utilities/connectDb.js";
+const db = getDb();
 
-async function verifyExistence(username) {
-  return false;
+async function verifyExistence(username, res) {
+  try {
+    //tries to search for the username in db
+    const userRef = doc(db, "users", username);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      res.status(400).send("User already exists");
+      return true;
+    }
+    return false;
+  } catch (err) {
+    res.status(500).send("Internal server error");
+    console.log(err);
+  }
 }
 
 async function addUserToDb(username, password, res) {
-  const db = getDb();
   await setDoc(doc(db, "users", username), {
     username: username,
     password: password,
@@ -16,8 +34,13 @@ async function addUserToDb(username, password, res) {
 
 async function registerNewUser(req, res) {
   const { username, password } = req.body;
-  await verifyExistence();
-  await addUserToDb(username, password, res);
+  try {
+    const existence = await verifyExistence(username, res);
+    console.log(existence);
+    if (!existence) await addUserToDb(username, password, res);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { registerNewUser };
